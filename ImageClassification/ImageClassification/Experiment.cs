89708d357e9ccffbd2b89e93fb7e26f74b8863bb
Aiman Zehra <aiman.-@stud.fra-uas.de>
2022-03-19 05:07:22 +0530

    Minor Changes in code

--- ImageClassification/ImageClassification/Experiment.cs
+++ ImageClassification/ImageClassification/Experiment.cs
@@ -165,7 +165,7 @@ namespace ConsoleApp
                         temp1 = avgSimilarity;
 
                         label = $"{"The image is predicted as " + secondEntry.Key}";
-                        if (temp1 < 60.0) //This depends and selected based on the HTM parameters given in htmconfig.json file
+                        if (temp1 < 65.0) //This depends and selected based on the HTM parameters given in htmconfig.json file
                         {
                             label = "The similarity of Input Image is too low, hence the given image might not belong to the Learning Dataset";
                         }
